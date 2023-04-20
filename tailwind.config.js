b5163c55243dbb3d8c2dfbce3a13117d41ae6822
Ryel Banfield <ryelbanfield@gmail.com>
2023-04-20 00:31:48 -0400

    Configure TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          Purple: "hsl(259, 100%, 65%)",
+          LightRed: "hsl(0, 100%, 67%)",
+        },
+        neutral: {
+          White: "hsl(0, 0%, 100%)",
+          OffWhite: "hsl(0, 0%, 94%)",
+          LightGrey: "hsl(0, 0%, 86%)",
+          SmokeyGrey: "hsl(0, 1%, 44%)",
+          OffBlack: "hsl(0, 0%, 8%)",
+        },
       },
     },
   },
   plugins: [],
-}
+};
